66c8af508cc60c2df8675e3f5055d6dc29ebfa17
Benidict Dulce <benidict.programmer@gmail.com>
2026-03-15 22:14:46 +0800

    create accounts folder

---
+++ ll_project/accounts/urls.py
@@ -0,0 +1,8 @@
+"""Defines url patterns for account"""
+
+from django.urls import path, include
+
+app_name = 'accounts'
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+]
--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'learning_logs',
     'django.contrib.admin',
     'django.contrib.auth',
