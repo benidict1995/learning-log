8b88a96e5a79d5276b934a72d14165576e9068d0
Benidict Dulce <benidict.programmer@gmail.com>
2026-03-15 15:55:46 +0800

    create a basic web app

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'll_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'll_project' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
