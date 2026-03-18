a61f7b9e7a60eb58b907881c31df5f1aa04142be
Benidict Dulce <benidict.programmer@gmail.com>
2026-03-18 22:24:08 +0800

    add security

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
